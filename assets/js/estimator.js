630639afc733d66708d3a337c0bc630b8a0c3e5b
Kevin Jin <kevindujin@berkeley.edu>
2021-02-14 09:41:00 -0800

    We are yeeted

--- assets/js/estimator.js
+++ assets/js/estimator.js
@@ -5,8 +5,8 @@ function select(id) {
 select("estimate").innerHTML = "Enter data for an estimate...";
 
 
-select("houses").onchange = function() {writeEstimate(updateCost())}
-select("furnishing").onchange = function() {writeEstimate(updateCost())}
+select("couch").onchange = function() {writeEstimate(updateCost())}
+select("couch_num").onchange = function() {writeEstimate(updateCost())}
 select("sqft").onchange = function() {writeEstimate(updateCost())}
 select("flooring").onchange = function() {writeEstimate(updateCost())}
 select("low").onchange = function() {writeEstimate(updateCost())}
@@ -14,15 +14,22 @@ select("high").onchange = function() {writeEstimate(updateCost())}
 // select("houses").onchange = function() {writeEstimate(updateCost())}
 
 
-const houses = [50, 500, 5000, 20000];
-const furnishing = [0, 500, 5000, 40000];
-const flooring = [0, 5, 20];
+const couch_low = [0, 200, 400, 600];
+const couch_high = [0, 400, 600, 1000]
+const flooring_low = [0, 6, 1];
+const flooring_high = [0, 12, 4];
 
 function updateCost() {
+    const offset = 0;
     let total = 0;
-    total += houses[select("houses").selectedIndex];
-    total += furnishing[select("furnishing").selectedIndex];
-    total += select("sqft").value * flooring[select("flooring").selectedIndex];
+    if (select("high").checked) {
+        total += couch_high[select("couch").selectedIndex] * select("couch_num").value;
+        total += select("sqft").value * flooring_high[select("flooring").selectedIndex];
+
+    } else {
+        total += couch_low[select("couch").selectedIndex] * select("couch_num").value;
+        total += select("sqft").value * flooring_low[select("flooring").selectedIndex];
+    }
     return total;
 }
 
